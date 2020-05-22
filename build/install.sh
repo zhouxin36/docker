497d247890785df5229d058a26ad4eba628ad2b0
zhouxin <1561578781@qq.com>
2020-05-22 11:26:34 +0800

    update install.sh

--- build/install.sh
+++ build/install.sh
@@ -5,7 +5,7 @@ yum -y install yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 # 安装docker
-yum install --nobest docker-ce docker-ce-cli
+yum -y install --nobest docker-ce docker-ce-cli
 # 安装固定版本container
 dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
 # 更新docker版本
