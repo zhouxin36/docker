8efdacaad2f28a27c6ffc1f504d359e1e4fdff9e
zhouxin <1561578781@qq.com>
2020-05-22 11:51:56 +0800

    update uninstall.sh

--- build/install.sh
+++ build/install.sh
@@ -9,6 +9,15 @@ yum -y install http://mirrors.aliyun.com/docker-ce/linux/centos/7/x86_64/stable/
 yum -y install http://mirrors.aliyun.com/docker-ce/linux/centos/7/x86_64/stable/Packages/docker-ce-19.03.8-3.el7.x86_64.rpm
 yum -y install http://mirrors.aliyun.com/docker-ce/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
 
+# 添加镜像加速器
+mkdir -p /etc/docker
+tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://6eb2jqvr.mirror.aliyuncs.com"]
+}
+EOF
+systemctl daemon-reload
+
 # 启动docker
 systemctl start docker
 
--- build/uninstall.sh
+++ build/uninstall.sh
@@ -2,3 +2,5 @@
 
 yum -y remove docker-ce docker-ce-cli containerd.io
 rm -rf /var/lib/docker
+rm -rf /etc/docker
+rm -rf /usr/libexec/docker
