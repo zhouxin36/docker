6cc0ef390c0b2b6eb092fc2aa8e2884baf99781f
zhouxin <1561578781@qq.com>
2020-05-22 14:48:06 +0800

    update go-test build.sh

--- go-test/build.sh
+++ go-test/build.sh
@@ -3,4 +3,5 @@
 
 # build
 docker build -t registry.cn-shenzhen.aliyuncs.com/zhouxin_go/my-go-test:latest .
-docker push
+echo "构建成功,push......"
+docker push registry.cn-shenzhen.aliyuncs.com/zhouxin_go/my-go-test:latest
