9c08dc5e0fde5136df786dc0112764e8d07803db
zhouxin <1561578781@qq.com>
2020-05-22 11:40:33 +0800

    add docker install.sh

--- build/install.sh
+++ build/install.sh
@@ -4,10 +4,12 @@
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 
-# 更新并安装 Docker-CE
-yum makecache fast
 # 安装docker
-yum -y install docker-ce docker-ce-cli containerd.io
+yum -y install --nobest docker-ce docker-ce-cli containerd.io
+# 安装固定版本container
+dnf -y install http://mirrors.aliyun.com/docker-ce/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+# 更新docker版本
+dnf -y update docker-ce docker-ce-cli
 
 # 启动docker
 systemctl start docker
