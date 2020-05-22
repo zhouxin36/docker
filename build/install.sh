988ccf19c210a5d36abe86c3fdde203d712437f5
zhouxin <1561578781@qq.com>
2020-05-22 11:24:17 +0800

    update docker/install.sh

--- build/install.sh
+++ build/install.sh
@@ -5,7 +5,11 @@ yum -y install yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 # 安装docker
-yum -y install docker-ce docker-ce-cli containerd.io
+yum install --nobest docker-ce docker-ce-cli
+# 安装固定版本container
+dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+# 更新docker版本
+dnf update docker-ce docker-ce-cli
 
 # 启动docker
 systemctl start docker
