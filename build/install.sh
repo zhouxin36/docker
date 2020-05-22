6998242f5b0bf6c7e7fa652791b57c1cbcc5e43f
zhouxin <1561578781@qq.com>
2020-05-22 14:12:59 +0800

    update install.sh

--- build/install.sh
+++ build/install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+MIRROR_URL=http://mirrors.aliyun.com/docker-ce/linux/centos
+MIRROR_PACKAGE=${MIRROR_URL}/7/x86_64/stable/Packages
+
 # 设置docker仓库
 yum install -y yum-utils device-mapper-persistent-data lvm2
-yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum-config-manager --add-repo ${MIRROR_URL}/docker-ce.repo
 
 # 安装固定版本container,docker-ce,docker-ce-cli
-yum -y install http://mirrors.aliyun.com/docker-ce/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
-yum -y install http://mirrors.aliyun.com/docker-ce/linux/centos/7/x86_64/stable/Packages/docker-ce-cli-19.03.8-3.el7.x86_64.rpm 
-yum -y install http://mirrors.aliyun.com/docker-ce/linux/centos/7/x86_64/stable/Packages/docker-ce-19.03.8-3.el7.x86_64.rpm
+yum -y install ${MIRROR_PACKAGE}/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+yum -y install ${MIRROR_PACKAGE}/docker-ce-cli-19.03.8-3.el7.x86_64.rpm 
+yum -y install ${MIRROR_PACKAGE}/docker-ce-19.03.8-3.el7.x86_64.rpm
 
 # 添加镜像加速器
 mkdir -p /etc/docker
