7c7635026313a801cc4f55425e590c00a12f51b8
zhouxin <1561578781@qq.com>
2020-06-06 23:22:30 +0800

    update sh

--- hadoop/source/hadoop_source.sh
+++ hadoop/source/hadoop_source.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# JAVA_HOME=/usr/local/openjdk-8
+# HADOOP_HOME=/hadoop
+
 # 下载hadoop
 wget -O hadoop-2.tar.gz -c https://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-2.7.7/hadoop-2.7.7.tar.gz
 
@@ -8,3 +11,9 @@ mkdir hadoop && tar -zvxf hadoop-2.tar.gz -C ./hadoop --strip-components 1
 
 # 删除压缩包
 rm -rf hadoop-2.tar.gz
+
+# 构建docker镜像
+docker build -t registry.cn-shenzhen.aliyuncs.com/zhouxin36/hadoop .
+
+# 上传docker镜像
+docker push registry.cn-shenzhen.aliyuncs.com/zhouxin36/hadoop
